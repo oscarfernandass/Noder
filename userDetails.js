eae8a69c7f06a55ab61825c5e3e7200d21ec54d1
oscarfernandass <125243046+oscarfernandass@users.noreply.github.com>
2024-01-17 17:27:27 +0530

    Update userDetails.js

--- userDetails.js
+++ userDetails.js
@@ -1,9 +1,24 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+// UserInfo Schema
 const userSchema = new Schema({
   message: String,
   date: String,
 });
 
 mongoose.model("UserInfo", userSchema);
+
+// Profile Card Schema
+const profileCardSchema = new Schema({
+  name: String,
+  id: String,
+  class: String,
+  number: String,
+  email: String,
+  parent: String,
+  contact: String,
+  address: String,
+});
+
+mongoose.model("ProfileCardInfo", profileCardSchema);
